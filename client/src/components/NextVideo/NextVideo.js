2222988dc7b0e49eced2cab70aa42ee15babfde4
Ana Olekas <86985160+ShianneOlekas@users.noreply.github.com>
2021-09-01 22:18:49 -0400

    added a scroll to top function, so when thumbnail in next videos list is clicked, it brings users back to the top of the page

--- client/src/components/NextVideo/NextVideo.js
+++ client/src/components/NextVideo/NextVideo.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 
 function NextVideo({ nextVideos }) {
 
+    function scrollToTop() {
+        window.scrollTo(0,0)
+    }
+
     const videoRendered = nextVideos?.map((nextVideo) => (
         <Link to={`/videos/${nextVideo.id}`} key={nextVideo.id}>
-            <div className='next-videos' >
+            <div className='next-videos' onClick={scrollToTop}>
                 <img className='next-videos__img' src={nextVideo.image} alt=''/>
                 <div className='next-info' >
                     <h3 className='next-info__title' >{nextVideo.title}</h3>
